d69ac2e8b97355ee8c6e88f966887403026be40d
sabbirhfm <144573428+sabbirhfm@users.noreply.github.com>
2025-01-21 01:49:19 -0800

    file upload

--- 21.01.2025/S.cpp
+++ 21.01.2025/S.cpp
@@ -5,24 +5,71 @@
 #define all(v) v.begin(),v.end()
 #define print(v) for(auto data : v) cout << data << " "; cout << nl
 using namespace std;
-const int MOD = 10E9 + 7;
+const long long MOD = 1E9 + 7;
+const ll n = 1E4;
+bool comp[n+1] = {0};
+//vector<int> di[n+1];
+vector<int> lp(n+1); // lowest prime divisor
 void solve()
 {
-    int n; cin >> n;
+    int N; cin >> N;
     ll fact = 1;
-    for(int i=2; i<=n; i++)
+    for(int i=2; i<=N; i++)
     {
-        //fact *= i;
-        fact = (1ll* fact%MOD * i%MOD) % MOD;
+        fact = (1ll * fact%MOD * i%MOD) % MOD;
     }
-    //cout << fact << nl;
 
-    int cnt = 0;
-    for(ll i=1; i<=fact; i++)
+    for(int i=1; i<=n; i++) lp[i] = 0;
+    for(int i=2; i <= n; i++) // i is a prime
     {
-        if(fact%i == 0) cnt++;
+        if(comp[i]) continue;
+        lp[i] = i;
+
+        for(int j=i+i; j<=n; j+=i)
+        {
+            comp[j] = true;
+            if(lp[j] == 0) lp[j] = i; // for lowest prime
+        }
     }
-    cout << cnt << nl;
+
+    cout << "fact of n: " << fact << nl;
+
+    for(int i=1; i<=10; i++)
+    {
+        if(lp[i] != 0)
+        {
+            cout << i << " -> " << lp[i] << nl;
+        }
+    }
+}
+void solve2()
+{
+    int N; cin >> N;
+
+    for(int i=2; i <= n; i++) // i is a prime
+    {
+        if(comp[i]) continue;
+        lp[i] = i;
+        for(int j=i+i; j<=n; j+=i)
+        {
+            comp[j] = true;
+            if(lp[j] == 0) lp[j] = i; // for lowest prime
+        }
+    }
+    map<ll,ll> mp;
+    for(int i=2; i<=N; i++)
+    {
+        ll x = i;
+        while(x != 1)
+        {
+            mp[lp[x]]++;
+            x /= lp[x];
+        }
+    }
+    ll ans = 1;
+    for(auto e:mp) ans *= (e.second+1) , ans %= MOD;
+    //ans = (ans+MOD)%MOD;
+    cout<<ans<<endl;
 }
 int main()
 {
@@ -30,8 +77,8 @@ int main()
 
     //int t; cin >> t;
     //while (t--) solve();
-    solve();
-
+    solve2();
 
     return 0;
 }
+
